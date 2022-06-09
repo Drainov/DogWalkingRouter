66cc392ff59d11bcc63fd348b6954ba2563610b7
Drainov <90571255+Drainov@users.noreply.github.com>
2022-06-09 16:29:26 +0200

    Update sql_batch_infilefolder.bat

--- SQL/OSM2PO_M/sql_batch_infilefolder.bat
+++ SQL/OSM2PO_M/sql_batch_infilefolder.bat
@@ -2,6 +2,6 @@ set PSQL="C:\Program Files\PostgreSQL\14\bin\psql"
 set PGPORT=5432
 set PGHOST=localhost
 set PGPASSWORD=#here your password
-cd hh
+cd foot
 %PSQL% -U postgres -d $Database name here$ -f "$filename of the sql file from osm2po$.sql"
 pause
