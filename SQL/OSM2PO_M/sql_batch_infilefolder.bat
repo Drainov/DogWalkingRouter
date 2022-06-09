b88cd726ee02ffd613b6f321c850f46510ab2a9e
Drainov <90571255+Drainov@users.noreply.github.com>
2022-06-09 16:10:36 +0200

    My OSM2PO settings
    test

---
+++ SQL/OSM2PO_M/sql_batch_infilefolder.bat
@@ -0,0 +1,7 @@
+set PSQL="C:\Program Files\PostgreSQL\14\bin\psql"
+set PGPORT=5432
+set PGHOST=localhost
+set PGPASSWORD=#here your password
+cd hh
+%PSQL% -U postgres -d $Database name here$ -f "$filename of the sql file from osm2po$.sql"
+pause
---
+++ SQL/OSM2PO_M/start_utrecht.bat
@@ -0,0 +1,1 @@
+java -Xmx8g -jar osm2po-core-5.5.2-signed.jar prefix=foot tileSize=x http://download.geofabrik.de/europe/netherlands-latest.osm.pbf postp.0.class=de.cm.osm2po.plugins.postp.PgRoutingWriter desktop
