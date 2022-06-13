51c2adad2ba7f8b00e62bcac9feb23fb9a55c9c4
Drainov <90571255+Drainov@users.noreply.github.com>
2022-06-13 16:07:18 +0200

    added geocoder

--- js/Routing.js
+++ js/Routing.js
@@ -13,7 +13,7 @@ var point_3 = null;
 var point_4 = null;
 var point_5 = null;
 
-//initialize map & sidebar
+//initialize map & sidebar + location
 var map = L.map('DogMap', { 
     center:[52, 5], 
     zoom:10}
@@ -29,6 +29,27 @@ var OpenStreetMap = L.tileLayer(
         attribution: '&copy; <a href="http:openstreetmap.org/copyright">OpenStreetMap</a>'
     }
 ).addTo(map);
+L.Control.geocoder().addTo(map);
+        if (!navigator.geolocation) {
+            console.log("Your browser doesn't support geolocation feature!")
+        } else {
+            setInterval(() => {
+                navigator.geolocation.getCurrentPosition(getPosition)
+            }, 5000);
+        };
+		var marker, lat, long;
+		function getPosition(position) {
+            // console.log(position)
+            lat = position.coords.latitude
+            long = position.coords.longitude
+            if (marker) {
+                map.removeLayer(marker)
+            }
+            marker = L.marker([lat, long])
+            L.featureGroup([marker]).addTo(map)
+			map.setView(([lat, long]),12);
+
+        }
 //Jquerry slide bar for distance
 $( function() {
     $( "#slider-range-max" ).slider({
@@ -61,72 +82,72 @@ $(document).ready(function(){
 		}
 		else if($(this).is(":checked")){
 			alert("Checkbox is checked.");
-// shortest path layer geojson
-var pathLayer = L.geoJSON(null);
+			// shortest path layer geojson
+			var pathLayer = L.geoJSON(null);
 
 
-// draggable marker for starting point. Note the marker is initialized with an initial starting position
-var sourceMarker = L.marker([52.089149, 5.142144], {
-	draggable: true
-})
-	.on("dragend", function(e) {
-		selectedPoint = e.target.getLatLng();
-		getVertex(selectedPoint);
-		getRoute();
-	})
-	.addTo(map);
+			// draggable marker for starting point. Note the marker is initialized with an initial starting position
+			var sourceMarker = L.marker([52.089149, 5.142144], {
+				draggable: true
+			})
+				.on("dragend", function(e) {
+					selectedPoint = e.target.getLatLng();
+					getVertex(selectedPoint);
+					getRoute();
+				})
+				.addTo(map);
 
-// draggbale marker for destination point.Note the marker is initialized with an initial destination positon
-var targetMarker = L.marker([52.092516, 5.116474], {
-	draggable: true
-})
-	.on("dragend", function(e) {
-		selectedPoint = e.target.getLatLng();
-		getVertex(selectedPoint);
-		getRoute();
-	})
-	.addTo(map);
+			// draggbale marker for destination point.Note the marker is initialized with an initial destination positon
+			var targetMarker = L.marker([52.092516, 5.116474], {
+				draggable: true
+			})
+				.on("dragend", function(e) {
+					selectedPoint = e.target.getLatLng();
+					getVertex(selectedPoint);
+					getRoute();
+				})
+				.addTo(map);
 
-// function to get nearest vertex to the passed point
-function getVertex(selectedPoint) {
-	var url = `${geoserverUrl}/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=cite:nearest_vertex&outputformat=application/json&viewparams=x:${
-		selectedPoint.lng};y:${selectedPoint.lat};`;
-	$.ajax({
-		url: url,
-		async: false,
-		success: function(data) {
-			loadVertex(
-				data,
-				selectedPoint.toString() === sourceMarker.getLatLng().toString()
-			);
-		}
-	});
-}
+			// function to get nearest vertex to the passed point
+			function getVertex(selectedPoint) {
+				var url = `${geoserverUrl}/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=cite:nearest_vertex&outputformat=application/json&viewparams=x:${
+					selectedPoint.lng};y:${selectedPoint.lat};`;
+				$.ajax({
+					url: url,
+					async: false,
+					success: function(data) {
+						loadVertex(
+							data,
+							selectedPoint.toString() === sourceMarker.getLatLng().toString()
+						);
+					}
+				});
+			}
 
-// function to update the source and target nodes as returned from geoserver for later querying
-function loadVertex(response, isSource) {
-	var features = response.features;
-	map.removeLayer(pathLayer);
-	if (isSource) {
-		source = features[0].properties.id;
-	} else {
-		target = features[0].properties.id;
-	}
-}
+			// function to update the source and target nodes as returned from geoserver for later querying
+			function loadVertex(response, isSource) {
+				var features = response.features;
+				map.removeLayer(pathLayer);
+				if (isSource) {
+					source = features[0].properties.id;
+				} else {
+					target = features[0].properties.id;
+				}
+			}
 
-// function to get the shortest path from the give source and target nodes
-function getRoute() {
-	var url = `${geoserverUrl}/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=cite:shortest_path&outputformat=application/json&viewparams=source:${source};target:${target};`;
+			// function to get the shortest path from the give source and target nodes
+			function getRoute() {
+				var url = `${geoserverUrl}/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=cite:shortest_path&outputformat=application/json&viewparams=source:${source};target:${target};`;
 
-	$.getJSON(url, function(data) {
-		map.removeLayer(pathLayer);
-		pathLayer = L.geoJSON(data);
-		map.addLayer(pathLayer);
-	});
-}
-getVertex(sourceMarker.getLatLng());
-getVertex(targetMarker.getLatLng());
-getRoute();
+				$.getJSON(url, function(data) {
+					map.removeLayer(pathLayer);
+					pathLayer = L.geoJSON(data);
+					map.addLayer(pathLayer);
+				});
+			}
+			getVertex(sourceMarker.getLatLng());
+			getVertex(targetMarker.getLatLng());
+			getRoute();
 }
 });
 });
