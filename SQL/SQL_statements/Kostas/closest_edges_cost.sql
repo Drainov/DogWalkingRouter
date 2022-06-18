2cc287f87692e8622d0de73de23ba63773326a69
Vk0z <96526485+Vk0z@users.noreply.github.com>
2022-06-18 21:04:54 +0300

    Create closest_edges_cost.sql

---
+++ SQL/SQL_statements/Kostas/closest_edges_cost.sql
@@ -0,0 +1,10 @@
+-- Selects the edges that are within a distance of 0.0003 of a greenspace and updates their cost on the edges table
+-- open_spaces_utrecht_splitted is the polygon layer with the greenspaces
+-- wkb_geometry is its geometry field
+
+with closest_edges as (select e.id, e.the_geom, o.ogc_fid
+from edges e
+inner join open_spaces_utrecht_splitted o
+on ST_DWithin(e.the_geom, o.wkb_geometry, 0.0003)
+)
+update edges set cost = cost/100 from closest_edges where closest_edges.id=dupe_edges.id
